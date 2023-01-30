baf6dc99d9e35dfa718d572a5065ed6d9339cc1d
OksanaN <nurova.oksana2020@gmail.com>
2023-01-30 18:30:23 +0500

    refactor

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -24,19 +24,42 @@ const Messege = (props: MessegeType) => {
     )
 }
 
-export const Dialogs = () => {
+export const Dialogs= () => {
+    type UserType = {
+        id: number,
+        name: string
+    }
+
+    type MessagesType = {
+        id: number
+        message: string
+    }
+
+    const users: UserType[] = [
+        {id: 1, name: 'Игорь'},
+        {id: 2, name: 'Марселин'},
+        {id: 3, name: 'Чуча'}
+    ]
+
+    const messages: MessagesType[] = [
+        {id: 1, message: 'Hey'},
+        {id: 2, message: 'Wazzap'},
+        {id: 3, message: 'U r hot'},
+        {id: 4, message: 'I love u'}
+    ]
+
     return (
         <div className={d.Dialogs}>
             <div className={d.DialogsItem}>
-                <Dialog id={1} name='Игорь'/>
-                <Dialog id={2} name='Марселин'/>
-                <Dialog id={3} name='Чуча'/>
+                <Dialog id={users[0].id} name={users[0].name}/>
+                <Dialog id={users[1].id} name={users[1].name}/>
+                <Dialog id={users[2].id} name={users[2].name}/>
             </div>
             <div className={d.MessegesItem}>
-                <Messege messege='Hey'/>
-                <Messege messege='Wazzap'/>
-                <Messege messege='U r hot'/>
-                <Messege messege='I love u'/>
+                <Messege messege={messages[0].message}/>
+                <Messege messege={messages[1].message}/>
+                <Messege messege={messages[2].message}/>
+                <Messege messege={messages[3].message}/>
             </div>
         </div>
     )
--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import p from './Profile.module.css'
-import {Post} from "./MyPosts/Post/Post";
-import {MyPosts} from "./MyPosts/MyPosts";
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 
 export function Profile () {
     return (
         <div className={p.content}>
             <ProfileInfo/>
-            <MyPosts/>
         </div>
     )
 }
