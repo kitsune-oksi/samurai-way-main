114505bee7e097068f47c021a7fb2cb749e7dd29
OksanaN <nurova.oksana2020@gmail.com>
2023-09-14 11:44:29 +0300

    add validation in form

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,5 +1,7 @@
 import {Field, InjectedFormProps, reduxForm} from "redux-form";
 import React from "react";
+import {maxLengthCreator, requiredField} from "../../utils/validators/valodators";
+import {Input} from "../common/FormsControl/FormsContol";
 
 type FormDataType = {
     login: string
@@ -7,17 +9,19 @@ type FormDataType = {
     rememberMe: boolean
 }
 
+const maxLength30 = maxLengthCreator(30);
+
 const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field placeholder='Login' component='input' name='login'/>
+                <Field placeholder='Login' component={Input} name='login' validate={[requiredField]}/>
             </div>
             <div>
-                <Field placeholder='Password' component='input' name='password'/>
+                <Field placeholder='Password' component={Input} name='password' validate={[requiredField, maxLength30]}/>
             </div>
             <div>
-                <Field type='checkbox' component='input' name='rememberMe'/> remember me
+                <Field type='checkbox' component={Input} name='rememberMe'/> remember me
             </div>
             <div>
                 <button>Login</button>
